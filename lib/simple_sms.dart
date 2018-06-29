d5bd361a032d31ac87b134822a6f332130da0766
Václav Chadt <vchadt@gmail.com>
2018-06-29 22:29:09 +0200

    WIP

--- lib/simple_sms.dart
+++ lib/simple_sms.dart
@@ -2,19 +2,31 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class SimpleSms {
-  static const MethodChannel _channel =
-      const MethodChannel('simple_sms');
 
-  static Future<String> get platformVersion async {
-    // @TODO ENUM function names
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+class SimpleSms {
+
+  static const _channelName =  'plugins.simple_sms.quichesoft.com/sendSms';
+  static const _methodName = 'sendSMS';
+
+  static const MethodChannel _channel =
+      const MethodChannel(_channelName);
+  RegExp exp = RegExp(r'/^[0-9 ()+-]+$/');
 
   Future<void> sendSms(List<String> addresses, String message) async {
-    // @TODO validation
 
-    await _channel.invokeMethod("sendSMS");
+
+    if (addresses.length == 0 || addresses == null){
+       throw 'Address must contain at least one item';
+    }
+
+    addresses.forEach((element){
+     if (exp.firstMatch(element) == null){
+       throw 'Addresses contain invalid character';
+     }
+    });
+
+    await _channel.invokeMethod(_methodName, [addresses, message]);
   }
+
+
 }
