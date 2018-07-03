428cf3ddb01141e192774c29ab124c90ba4fa432
vchadt <vchadt@gmail.com>
2018-07-03 17:05:00 +0200

    specify sdk version

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,9 +5,6 @@
 // are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:simple_sms_example/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
